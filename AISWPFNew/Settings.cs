f3b848c540576805266e5fa4709c72f34ee7b78e
rj128x <rj128x@gmail.com>
2017-09-14 12:55:58 +0500

    159

--- AISWPFNew/Settings.cs
+++ AISWPFNew/Settings.cs
@@ -6,12 +6,13 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Controls;
 using System.Xml.Serialization;
 
-namespace KotmiLib
+namespace AISWPF
 {
 	public class XMLSer<T>
 	{
@@ -43,23 +44,63 @@ namespace KotmiLib
 		public String Server { get; set; }
 		public String User { get; set; }
 		public string Password { get; set; }
-		public List<String> KotmiFields { get; set; }
-		public String DBServer { get; set; }
-		public String DBName { get; set; }
-		public String DBUser { get; set; }
-		public String DBPassword { get; set; }
-
+		public string DBName { get; set; }
+		public string SMTPServer { get; set; }
+		public string SMTPUser { get; set; }
+		public string SMTPFrom { get; set; }
+		public string SMTPTo { get; set; }
+		public string SMTPDomain { get; set; }
+		public string SMTPPassword { get; set; }
+		public int SMTPPort { get; set; }
+		public List<string> Sources { get; set; }
 
 		public static Settings Single { get; protected set; }
 		public static void init(string filename) {
 			try {
-				Settings single = XMLSer<Settings>.fromXML( filename);
+				Settings single = XMLSer<Settings>.fromXML(filename);
 				Single = single;
-				//single.KotmiFields = new List<string>();
-				/*single.KotmiFields.Add("214235345");
-				XMLSer<Settings>.toXML(single, "C:/test.xml");*/
+
 			} catch (Exception e) {
 				Logger.error("Ошибка при чтении файла настроек " + e, Logger.LoggerSource.server);
+			}
+		}
+	}
+
+	public class MailClass
+	{
+		public static void sendMail(string message,string header) {
+			try {
+				string body = message;
+
+
+				System.Net.Mail.MailMessage mess = new System.Net.Mail.MailMessage();
+
+				mess.From = new MailAddress(Settings.Single.SMTPFrom);
+				mess.Subject = header;
+				mess.Body = body;
+
+				string[] addr = Settings.Single.SMTPTo.Split(new char[] { ';' });
+				foreach (string add in addr) {
+					try {
+						mess.To.Add(add);
+					} catch { }
+				}
+
+				mess.SubjectEncoding = System.Text.Encoding.UTF8;
+				mess.BodyEncoding = System.Text.Encoding.UTF8;
+				mess.IsBodyHtml = true;
+				System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient(Settings.Single.SMTPServer, Settings.Single.SMTPPort);
+				client.EnableSsl = true;
+				if (string.IsNullOrEmpty(Settings.Single.SMTPUser)) {
+					client.UseDefaultCredentials = true;
+				} else {
+					client.Credentials = new System.Net.NetworkCredential(Settings.Single.SMTPUser, Settings.Single.SMTPPassword, Settings.Single.SMTPDomain);
+				}
+				// Отправляем письмо
+				client.Send(mess);
+
+			} catch (Exception e) {
+				Logger.info(String.Format("Ошибка при отправке почты: {0}", e.ToString()), Logger.LoggerSource.server);
 			}
 		}
 	}
@@ -68,7 +109,7 @@ namespace KotmiLib
 	{
 		public enum LoggerSource { server, client, ordersContext, objectsContext, usersContext, service }
 		public static log4net.ILog logger;
-		public static RichTextBox TxtLog { get; set; }
+		public static TextBox rbText;
 		static Logger() {
 
 		}
@@ -85,11 +126,10 @@ namespace KotmiLib
 		}
 
 		public static string createMessage(string message, LoggerSource source) {
-			string msg= String.Format("{0,-20} {1}", DateTime.Now.ToString(), message);
-			if (TxtLog!=null)
-				TxtLog.Text = msg + "\r\n" + TxtLog.Text;
+			string msg = String.Format("{1}", source.ToString(), message);
 			try {
-				Console.WriteLine(msg);
+				rbText.Text = msg + "\r\n" + rbText.Text;
+				rbText.InvalidateArrange();
 			} catch { }
 			return msg;
 		}
